d6f2e244632aa74ce079dc235b590e9cd995c6b2
iomar <iomarbn@gmail.com>
2023-06-05 13:49:08 -0600

    Add Banks repository definition and empty implementation

---
+++ PagaTodoTest/PagaTodoTest/Model/Repository/BanksRepository.swift
@@ -0,0 +1,12 @@
+//
+//  BanksRepository.swift
+//  PagaTodoTest
+//
+//  Created by Omar Bacilio on 05/06/23.
+//
+
+import Foundation
+
+protocol BanksRepository {
+    
+}
---
+++ PagaTodoTest/PagaTodoTest/Model/Repository/BanksRepositoryImpl.swift
@@ -0,0 +1,12 @@
+//
+//  BanksRepositoryImpl.swift
+//  PagaTodoTest
+//
+//  Created by Omar Bacilio on 05/06/23.
+//
+
+import Foundation
+
+struct BanksRepositoryImpl: BanksRepository {
+    
+}
