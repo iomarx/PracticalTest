e5533fb9da7e07c7484576627e80ffa15b24f318
iomar <iomarbn@gmail.com>
2023-06-05 13:35:19 -0600

    Add Bank struct model

---
+++ PagaTodoTest/PagaTodoTest/Model/Bank.swift
@@ -0,0 +1,15 @@
+//
+//  Bank.swift
+//  PagaTodoTest
+//
+//  Created by Omar Bacilio on 05/06/23.
+//
+
+import Foundation
+
+struct Bank {
+    let bankName: String
+    let description: String
+    let age: Int
+    let url: String
+}
