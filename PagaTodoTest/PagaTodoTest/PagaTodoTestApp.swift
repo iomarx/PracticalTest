6285022fdc85997f900d3f1fa0a208afa81bb1ad
iomar <iomarbn@gmail.com>
2023-06-05 13:33:41 -0600

    Initial Commit
    Added project structure

---
+++ PagaTodoTest/PagaTodoTest/PagaTodoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagaTodoTestApp.swift
+//  PagaTodoTest
+//
+//  Created by Omar Bacilio on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PagaTodoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
