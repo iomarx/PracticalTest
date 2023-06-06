710e55bbb58b1af77c1c4fa6846fe8a800a25f10
iomar <iomarbn@gmail.com>
2023-06-05 19:13:00 -0600

    Add function to Map Model to Entity

--- PagaTodoTest/PagaTodoTest/Model/DataSource/CoreDataDataSource.swift
+++ PagaTodoTest/PagaTodoTest/Model/DataSource/CoreDataDataSource.swift
@@ -29,20 +29,13 @@ struct CoreDataDataSource: LocalDataSource {
     
     func save(_ bank: Bank) {
         let newItem = BankEntity(context: context)
-        newItem.id = bank.id
-        newItem.bankName = bank.bankName
-        newItem.bankDescription = bank.description
-        newItem.age = Int32(bank.age)
-        newItem.url = bank.url
-
+        newItem.fromBankModel(bank)
+        
         do {
             try context.save()
             print("Item Saved!")
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            print("Error on save Bank = \(error)")
         }
     }
 }
@@ -53,5 +46,13 @@ extension BankEntity {
              description: self.bankDescription ?? "",
              age: Int(self.age),
              url: self.url ?? "")
+    }
+    
+    func fromBankModel(_ bank: Bank) {
+        self.id = bank.id
+        self.bankName = bank.bankName
+        self.bankDescription = bank.description
+        self.age = Int32(bank.age)
+        self.url = bank.url
     }
 }
